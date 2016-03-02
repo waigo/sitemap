71b444a666395944c3c22bac7f3bbdd0222ab08c
Ramesh Nair <ram@hiddentao.com>
2016-03-02 15:38:10 +0800

    initial submission

---
+++ README.md
@@ -0,0 +1,32 @@
+This [waigo](http://waigojs.com) plugin provides:
+
+* Cron job to submit sitemap to Google and Bing.
+
+## Installation
+
+```bash
+$ npm install waigo-sitemap
+```
+
+## Usage
+
+The cron job is enabled by default and will run daily.
+
+The sitemap is expected to be found at `{app.config.baseURL}/sitemap.xml`. You 
+can change this in your config file:
+
+
+```javascript
+module.exports = function(config) {
+  ...
+
+  config.sitemapPath = '/custom_sitemap.xml';  // relative to baseURL
+
+  ...
+}
+```
+
+
+## License
+
+MIT - see LICENSE.md
---
+++ src/support/cron/submitSitemap.js
@@ -0,0 +1,32 @@
+const got = require('got');
+
+
+const pingUrl = function(app, url) {
+  app.logger.debug('Submitting sitemap: ' + url);
+
+  return got(url)
+    .then(function(res) {
+      if (200 !== res.status) {
+        throw new Error('Error fetching ' + url + ': ' + res.text);
+      }
+    })
+    .catch(function(err) {
+      app.logger.error(err.toString());
+    });
+};
+
+
+
+module.exports = {
+  schedule: '0 0 0 * * *',  // every day at midnight
+  handler: function*(app) {
+    var sitemapUrl = encodeURIComponent(
+      app.config.baseUrl + (app.config.sitemapPath || '/sitemap.xml')
+    );
+
+    yield [
+      pingUrl(app, 'http://www.google.com/ping?sitemap=' + sitemapUrl),
+      pingUrl(app, 'http://www.bing.com/ping?sitemap=' + sitemapUrl),
+    ];      
+  }
+};
